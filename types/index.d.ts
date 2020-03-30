e5c136871e49bf2d0298e844e584f7f235caffa1
Jasper van Herpt <jasper.van.herpt@omines.com>
2020-03-30 13:55:47 +0200

    Extend type definitions for the platform SDK

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,28 @@
 /// <reference types="node" />
 
 import {EventEmitter} from "events";
+import {ValidationResult} from "@hapi/joi";
+import {Logger} from "winston";
 
 declare namespace panflux {
-    interface Platform extends EventEmitter {
-        reportDiscovery(entity: Entity): boolean;
+    class Platform extends EventEmitter {
+        constructor(config: any, rootdir: string);
+
+        run(logTransport: any|any[]): void;
+
+        buildEntityTypes(types: any): void;
+        getEntityType(type: string): EntityType;
+        validateEntity(definition: any): ValidationResult;
+
+        config: any;
+        types: EntityType[];
+        name: string;
+        friendlyName: string;
+        rootdir: string;
+        version?: string;
+        versionURL?: string;
+
+        static load(rootdir: string): Platform;
     }
 
     interface EntityDeclaration {
@@ -15,8 +33,36 @@ declare namespace panflux {
         attributes?: Map<string, any>;
     }
 
-    interface Entity {
-        setState(name: string, value: number): number;
+    class EntityType {
+        constructor(name: string, definition: any);
+
+        createEntity(definition: any, platform: Platform, logger: Logger): Entity;
+        registerChildEntityType(name: string, definition: any): void;
+
+        validateDelta(delta: any): ValidationResult;
+        validateEntity(entity: any): ValidationResult
+
+        name: string;
+    }
+
+    class Entity {
+        constructor(definition: any, type: EntityType, platform: Platform, logger: Logger);
+
+        registerChildEntity(object: any): boolean;
+
+        emit(event: string, parameters: any): void
+        emit(event: string): void
+
+        setAttribute(name: string, value: any): void;
+        setAttributes(object: any): void;
+
+        setProperty(name: string, value: any): void;
+        setProperties(object: any): void;
+
+        id: string;
+        name: string;
+        type: EntityType;
+        config: any;
     }
 }
 
